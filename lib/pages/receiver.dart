5b2ddc8bdedf39396ae105fba63230c4899845eb
aumZax <162749933+aumZax@users.noreply.github.com>
2024-10-24 14:56:38 +0700

    Add files via upload

--- lib/pages/receiver.dart
+++ lib/pages/receiver.dart
@@ -511,6 +511,7 @@ class _ReceiverPageState extends State<ReceiverPage> {
               LatLng(user.senderLat, user.senderLong); // ใช้ข้อมูล GPS จาก API
           re = LatLng(
               user.senderLat, user.senderLong); // จุดปลายทาง (ตัวอย่างพิกัด)
+
         });
         log('aaaaaaaa: $senderId');
       } else {
@@ -618,7 +619,7 @@ class _ReceiverPageState extends State<ReceiverPage> {
       barrierDismissible: true, // สามารถกดด้านนอกเพื่อปิดได้
       builder: (BuildContext context) {
         return Dialog(
-          backgroundColor: Colors.transparent, // ตั้งค่าพื้นหลังเป็นโปร่งใส
+            backgroundColor: Colors.transparent, // ตั้งค่าพื้นหลังเป็นโปร่งใส
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(10.0),
           ),
